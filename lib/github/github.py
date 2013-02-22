437cd87637189a724c97d6124185df3c3ae8db9d
kordless <kordless@gmail.com>
2013-02-22 09:05:58 -0800

    whoop.

--- lib/github/github.py
+++ lib/github/github.py
@@ -134,7 +134,7 @@ def get_gist_content(gist_id):
         return content
     else:
         logging.info("cache miss for %s" % gist_id)
-        Try:
+        try:
             # go fetch the gist using the gist_id
             http = httplib2.Http(cache=None, timeout=10, proxy_info=None)
             headers, content = http.request('https://api.github.com/gists/%s' % gist_id, method='GET', body=None, headers=None)
